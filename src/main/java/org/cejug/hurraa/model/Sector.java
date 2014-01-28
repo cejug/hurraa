210d75c3e67ebffc2e5aed840b74b74ddc89c44c
Daniel Cunha <danielsoro@gmail.com>
2014-01-27 23:51:27 -0300

    Change validator to javax.validation

--- src/main/java/org/cejug/hurraa/model/Sector.java
+++ src/main/java/org/cejug/hurraa/model/Sector.java
@@ -5,8 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Sector {
@@ -15,7 +14,7 @@ public class Sector {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @NotNull
     @Column(name = "name")
     private String name;
 

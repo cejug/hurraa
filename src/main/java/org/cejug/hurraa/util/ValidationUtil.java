7f23835b825441eefb1aea4b21cb7977f04f88f8
Daniel Cunha <danielsoro@gmail.com>
2014-01-31 20:42:29 -0300

    Create produces to javax.validation.Validator

--- src/main/java/org/cejug/hurraa/util/ValidationUtil.java
+++ src/main/java/org/cejug/hurraa/util/ValidationUtil.java
@@ -3,37 +3,42 @@ package org.cejug.hurraa.util;
 import java.util.Set;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 import javax.validation.Path;
-import javax.validation.Validation;
-import javax.validation.ValidatorFactory;
+
+import org.cejug.hurraa.qualifiers.JavaxValidator;
 
 import br.com.caelum.vraptor.validator.SimpleMessage;
 import br.com.caelum.vraptor.validator.Validator;
 
-
 @Stateless
 public class ValidationUtil {
-	
-	public Validator validateBean( String prefix , Object validationTarget , Validator validator ){
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		javax.validation.Validator beanValidator = factory.getValidator();
-    	Set<ConstraintViolation<Object>> constrainsErrors =  beanValidator.validate(validationTarget);
-    	
-    	prefix = updatePrefix(prefix);
-    	for (ConstraintViolation<Object> constrain : constrainsErrors) {
-    		Path propertyPath = constrain.getPropertyPath();
-    		String category = prefix + propertyPath.toString();
-			validator.add( new SimpleMessage( category , constrain.getMessage() ));
+
+	@Inject
+	@JavaxValidator
+	private javax.validation.Validator beanValidator;
+
+	public Validator validateBean(String prefix, Object validationTarget,
+			Validator validator) {
+
+		Set<ConstraintViolation<Object>> constrainsErrors = beanValidator
+				.validate(validationTarget);
+
+		prefix = updatePrefix(prefix);
+		for (ConstraintViolation<Object> constrain : constrainsErrors) {
+			Path propertyPath = constrain.getPropertyPath();
+			String category = prefix + propertyPath.toString();
+			validator.add(new SimpleMessage(category, constrain.getMessage()));
 		}
 		return validator;
 	}
-	
-	protected String updatePrefix(String prefix){
-		if(prefix != null && !prefix.trim().isEmpty()){
+
+	protected String updatePrefix(String prefix) {
+		if (prefix != null && !prefix.trim().isEmpty()) {
 			return prefix + ".";
 		}
 		return "";
 	}
-	
+
 }

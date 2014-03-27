0d6a4efbc5ce58c9221c9ec53f12d77dceec3436
Paulo Jr <paulojribp@gmail.com>
2014-03-27 12:41:01 -0300

    Updating VRaptor to RC2 to close #128

--- src/main/java/org/cejug/hurraa/validation/CejugValidator.java
+++ src/main/java/org/cejug/hurraa/validation/CejugValidator.java
@@ -1,30 +1,33 @@
 /*
-* Hurraa is a web application conceived to manage resources
-* in companies that need manage IT resources. Create issues
-* and purchase IT materials. Copyright (C) 2014 CEJUG.
-*
-* Hurraa is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Hurraa is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Hurraa. If not, see http://www.gnu.org/licenses/gpl-3.0.html.
-*
-*/
+ * Hurraa is a web application conceived to manage resources
+ * in companies that need manage IT resources. Create issues
+ * and purchase IT materials. Copyright (C) 2014 CEJUG.
+ *
+ * Hurraa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hurraa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hurraa. If not, see http://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ */
 package org.cejug.hurraa.validation;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Specializes;
 import javax.inject.Inject;
+import javax.validation.MessageInterpolator;
+import javax.validation.Validator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,10 +41,10 @@ import br.com.caelum.vraptor.validator.Message;
 import br.com.caelum.vraptor.validator.Outjector;
 import br.com.caelum.vraptor.view.ValidationViewsFactory;
 
-@Specializes 
+@Specializes
 @RequestScoped
 public class CejugValidator extends DefaultValidator {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(DefaultValidator.class);
 
 	private final Result result;
@@ -50,12 +53,13 @@ public class CejugValidator extends DefaultValidator {
 	private final Proxifier proxifier;
 
 	protected CejugValidator() {
-		this(null, null, null, null, null);
+		this(null, null, null, null, null, null, null, null);
 	}
 
 	@Inject
-	public CejugValidator(Result result, ValidationViewsFactory factory, Outjector outjector, Proxifier proxifier, ResourceBundle bundle) {
-		super(  result , factory , outjector , proxifier , bundle );
+	public CejugValidator(Result result, ValidationViewsFactory factory, Outjector outjector, Proxifier proxifier,
+			ResourceBundle bundle, Validator validator, MessageInterpolator interpolator, Locale locale) {
+		super(result, factory, outjector, proxifier, bundle,  validator, interpolator, locale);
 		this.result = result;
 		this.viewsFactory = factory;
 		this.outjector = outjector;
@@ -67,11 +71,11 @@ public class CejugValidator extends DefaultValidator {
 		if (!hasErrors()) {
 			return new MockResult(proxifier).use(view); //ignore anything, no errors occurred
 		}
-		
+
 		List<Message> errors = getErrors();
 		result.include("errors", new CejugErrorMap( getErrors() ) );
 		outjector.outjectRequestMap();
-		
+
 		logger.debug("there are errors on result: {}", errors);
 		return viewsFactory.instanceFor(view, errors);
 	}
